34cc354863a252090b42ea639901bdc477cd8539
Dell <singh.pankaj@jarvis.consulting>
2025-05-22 17:48:48 +0530

    updates

--- goRoutine.go
+++ goRoutine.go
@@ -21,6 +21,15 @@ func sum(s []int, c chan int){
 	c<-sum // send sum to c
 }
 
+func fibonacci(n int,c  chan int){
+	x,y:=0,1
+	for i:=0;i<n;i++{
+		c<-x 
+		x,y=y,x+y
+	}
+	close(c)
+}
+
 
 
 func main(){
@@ -43,7 +52,22 @@ func main(){
 	fmt.Println(<-ch)
 	fmt.Println(<-ch)
 
+
+	d:=make(chan int,10)
+
+	go fibonacci(cap(d),d)
 	
+	for i:=range d{
+		fmt.Println(i)
+	}
+
+
+
+
+
+
+	
+
 
 
 
