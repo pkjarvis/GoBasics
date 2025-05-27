53fc3a9c1d02fbc37f18a8542d14c8c61b8f8f38
Dell <singh.pankaj@jarvis.consulting>
2025-05-27 10:31:22 +0530

    updates

--- oneshot.go
+++ oneshot.go
@@ -2,30 +2,50 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	// "strings"
 	// "unicode/utf8"
 	// "errors"
 	// "math/rand"
-	"time"
-	"sync"
+	// "time"
+	// "sync"
 )
 
-var wg=sync.WaitGroup{}
-var dbData=[]string{"id1","id2","id3","id4","id5"}
-var results=[]string{}
-var m=sync.RWMutex{}
+// var wg=sync.WaitGroup{}
 
-func dbCall(i int){
-	var delay float32=2000
-	time.Sleep(time.Duration(delay)*time.Millisecond)
-	fmt.Println("The result from the database is:",dbData[i])
+// var MAX_CHICKEN_PRICE float32=5
+// var dbData=[]string{"id1","id2","id3","id4","id5"}
+// var results=[]string{}
+// var m=sync.Mutex{}
+// var v=sync.RWMutex{}
 
-	m.Lock()
-	results=append(dbData,dbData[i])
-	m.Unlock()
+// func dbCall(i int){
+// 	// Simulate Db call delay
+// 	var delay float32=2000
+// 	time.Sleep(time.Duration(delay)*time.Millisecond)  // it pauses the latest go routine for provided time delay
+// 	fmt.Println("The result from the database is:",dbData[i])
+// 	save(dbData[i])
+// 	log()
 
-	wg.Done()
-}
+// 	m.Lock()
+// 	results=append(results,dbData[i])
+// 	m.Unlock()
+
+// 	wg.Done()
+// }
+
+
+
+// func log(){
+// 	v.RLock()
+// 	fmt.Println("\nThe current results are:%v",results)
+// 	v.RUnlock()
+// }
+
+// func save(result string){
+// 	m.Lock()
+// 	results=append(results,result)
+// 	m.Unlock()
+// }
 
 
 // func printMe(printVal string){
@@ -53,6 +73,15 @@ func dbCall(i int){
 // 	default:
 // 		fmt.Printf("The result of the integer division is%v with remainder %v",result,remainder)
 //     }
+// }
+
+
+// func count(){
+// 	var res int
+// 	for i:=0;i<100000000;i++{
+// 		res+=1;
+// 	}
+// 	wg.Done()
 // }
 
 
@@ -106,58 +135,113 @@ func main(){
 
 
 
-	arr:=[...]int64{1,2,3}
-	fmt.Println(arr)
+	// arr:=[...]int64{1,2,3}
+	// fmt.Println(arr)
 
 
-	var Mymap map[string]uint8=make(map[string]uint8)
-	fmt.Println(Mymap)
+	// var Mymap map[string]uint8=make(map[string]uint8)
+	// fmt.Println(Mymap)
 
-	var myMap2 = map[string]uint8{"Adam":23,"Sarah":22}
-	fmt.Println(myMap2["Adam"])
-	fmt.Println(myMap2["Sarah"])
+	// var myMap2 = map[string]uint8{"Adam":23,"Sarah":22}
+	// fmt.Println(myMap2["Adam"])
+	// fmt.Println(myMap2["Sarah"])
 	
 
-	var age,ok=myMap2["Jason"]
-	if ok{
-		fmt.Println("The age is %v",age)
-	}else{
-		fmt.Println("Invalid name")
-	}
+	// var age,ok=myMap2["Jason"]
+	// if ok{
+	// 	fmt.Println("The age is %v",age)
+	// }else{
+	// 	fmt.Println("Invalid name")
+	// }
 
-	for name,age:=range myMap2{
-		fmt.Printf("Name:%v , Age:%v \n",name,age)
-	}
-
-
-	var myRune='a'
-	fmt.Printf("myRune=%v",myRune)
-
-	var strSlice=[]string{"s","u","b","t","c","r","i"}
-	var b strings.Builder 
-	for i:= range strSlice{
-		b.WriteString(strSlice[i])
-	}
-
-	var catstr=b.String()
-	fmt.Printf("%v\n",catstr)
+	// for name,age:=range myMap2{
+	// 	fmt.Printf("Name:%v , Age:%v \n",name,age)
+	// }
 
 
+	// var myRune='a'
+	// fmt.Printf("myRune=%v",myRune)
+
+	// var strSlice=[]string{"s","u","b","t","c","r","i"}
+	// var b strings.Builder 
+	// for i:= range strSlice{
+	// 	b.WriteString(strSlice[i])
+	// }
+
+	// var catstr=b.String()
+	// fmt.Printf("%v\n",catstr)
+
+	// fmt.Println("end")
+	// t0:=time.Now()
+	// for i:=0;i<1000;i++{
+	// 	wg.Add(1)
+	// 	go count()
+	// }
+	// wg.Wait()
+	// fmt.Println("\nTotal execution time:%v",time.Since(t0))
 
 
+	// var c=make(chan int)
+	// c<-1
+	// var i=<-c   // this blocks the code execution and hence gives deadlock error
+	// fmt.Println(i)
+
+	// go process(c)
+	// for i:=range c{
+	// 	fmt.Println(i)
+	// 	time.Sleep(time.Second*1)
+	// }
+
+	// var chickenChannel=make(chan string)
+	// var websites=[]string{"walmart.com","costco.com","wholefoods.com"}
+	// for i:=range websites{
+	// 	go checkChickenPrices(websites[i],chickenChannel)
+	// }
+	// sendMessage(chickenChannel)
 
 
+	var intSlice=[]int{1,2,3}
+	fmt.Println(sumSlice[int](intSlice))
 
-
-
+	var float32Slice=[]float32{1,2,3}
+	fmt.Println(sumSlice[float32](float32Slice))
 
 
 
 	
-	  
+	
+}
+
+// func process(c chan int){
+// 	defer close(c)
+// 	for i:=0;i<5;i++{
+// 		c<-i
+// 	}
+// 	// close(c)
+// 	fmt.Println("existing process")
+// }
 
 
+// func checkChickenPrices(website string,chickenChannel chan string){
+// 	for{
+// 		time.Sleep(time.Second*1)
+// 		var checkChickenPrice=rand.Float32()*20
+// 		if checkChickenPrice<=MAX_CHICKEN_PRICE{
+// 			chickenChannel<-website 
+// 			break
+// 		}
+// 	}
+// }
 
+// func sendMessage(chickenChannel chan string){
+// 	fmt.Printf("\nFound a deal on chicken at %s",<-chickenChannel)
+// }
 
+func sumSlice[T int|float32|float64](slice []T)T{
+	var sum T
+	for _,v:=range slice{
+		sum+=v
+	}
+	return sum
 
 }
